0e864f32ee0b5230a876d13946a5682ad58a4d87
David Anitoiu <david.anitoiu@gmail.com>
2020-12-18 22:10:03 +0100

    Added test for homepage

--- __tests__/homepage.test.tsx
+++ __tests__/homepage.test.tsx
@@ -3,25 +3,43 @@ import * as React from 'react';
 import Homepage from '../pages/index';
 
 const setup = () => {
-    const { getByText, container } = render(<Homepage />);
+    const { container } = render(<Homepage />);
 
-    const header = getByText(/Welcome/);
-    const grid = container.querySelector('#grid');
-    const footer = container.querySelector('footer');;
+    const form = container.querySelector('form') as HTMLFormElement;
 
     return {
-        header,
-        grid,
-        footer
+        form
     }
 }
 
 describe('Homepage', () => {
-    it('should render a title, a grid and a footer', () => {
-        const { header, grid, footer } = setup();
+    it('should render a form', () => {
+        const { form } = setup();
 
-        expect(header).toBeInTheDocument();
-        expect(grid).toBeInTheDocument();
-        expect(footer).toBeInTheDocument();
+        expect(form).toBeInTheDocument();
+    });
+
+    it('should have 9 fieldsets', () => {
+        const { form } = setup();
+
+        const formDefaultValues = {
+            "customer-segments": "",
+            "early-adopters": "",
+            "problem": "",
+            "existing-alternatives": "",
+            "revenue-streams": "",
+            "solution": "",
+            "unique-value-proposition": "",
+            "high-level-concept": "",
+            "channels": "",
+            "key-metrics": "",
+            "cost-structure": "",
+            "unfair-advantage": ""
+          }
+
+        const formData = new FormData(form);
+        const formValues = Object.fromEntries(formData.entries());
+        
+        expect(formValues).toStrictEqual(formDefaultValues);
     })
 })
