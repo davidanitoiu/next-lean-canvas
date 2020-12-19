5ac8ea5f46a4cda59fea9c4c6db922865dbf31c0
David Anitoiu <david.anitoiu@gmail.com>
2020-12-19 14:43:42 +0100

    Added animxyz
    * extended HTMLAttributes to accept xyz

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import type { AppProps } from 'next/app';
 import "styles/globals.css";
 import "tailwindcss/tailwind.css";
+import '@animxyz/core'
+
+declare module 'react' {
+  interface HTMLAttributes<T> {
+    xyz?: string
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
